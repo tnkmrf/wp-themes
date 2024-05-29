c3b9773460fef8a50ed0d4c7b3ca3bd325f02639
tnkmrf <tnkmrf@gmail.com>
2024-05-29 15:42:04 +0200

    feat: custom post colour

--- Twopointoh/wp-content/themes/twopointoh/components/footer.php
+++ Twopointoh/wp-content/themes/twopointoh/components/footer.php
@@ -7,6 +7,21 @@ $contact_link = get_field('contact_link',$menu);
 <footer>
     <div class="footer-container">
         <div class="footer-content normal-width">
+            <div class="footer-nav">
+                <div class="footer-nav-items">
+                <?php
+                wp_nav_menu(
+
+                array(
+                    'theme_location' => 'mt-menu'
+                )
+
+                );
+
+
+                ?>
+                </div>
+            </div>
             <div class="footer-cta">
                 <p>Don't wait any longer,</p>
                 <a href="<?php echo esc_html($contact_link['url'])?>"><?php echo esc_html($contact_link['title'])?><span><img src="<?php echo get_template_directory_uri()?>/resources/img/icons/arrow-right-footer.svg" alt="logo"></span></a>
--- Twopointoh/wp-content/themes/twopointoh/single.php
+++ Twopointoh/wp-content/themes/twopointoh/single.php
@@ -19,16 +19,26 @@ while (have_posts()):
     $category_name = $category[0]->name;
     $category_link = get_category_link($category);
     $thumbnail = get_the_post_thumbnail_url();
+    $main_colour = get_field('post_main_colour');
     
     ?>
     <article>
-    <div class ="post-header">
+    <div class ="post-header" style="background:<?php echo esc_html($main_colour)?>">
         <div class="post-details ">
             <a class ="category-link" href = "<?php echo esc_url($category_link)?>"><?php esc_html_e($category_name)?></a>
             <h1 onclick="myFunction()"><?php esc_html_e($title)?></h1>
             <div class="post-excerpt">
             <?php echo wp_kses_post( get_the_excerpt());?>
             </div>
+            <?php if (!empty(get_the_tag_list())){
+            ?>
+            <div class="post-tags">
+             
+                <?php echo get_the_tag_list('<ul><li>', '</li><li>', '</li></ul>') ?>
+            </div>
+            <?php
+            };
+            ?>
             
         </div>
         <?php
@@ -50,15 +60,7 @@ while (have_posts()):
         
     </div>
 
-    <?php if (!empty(get_the_tag_list())){
-        ?>
-    <div class="post-tags normal-width">
-        <h3><?php esc_html_e('Tagged' )?></h3>
-        <?php echo get_the_tag_list('<ul><li>', '</li><li>', '</li></ul>') ?>
-    </div>
-    <?php
-    };
-    ?>
+
 
     </article>
 <?php
