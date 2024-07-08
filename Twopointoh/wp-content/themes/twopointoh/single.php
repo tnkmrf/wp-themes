4b6a159415c63257885892d12c70b0d6b006eae8
tnkmrf <tnkmrf@gmail.com>
2024-07-08 22:39:34 +0200

    feat: recomended posts

--- Twopointoh/wp-content/themes/twopointoh/single.php
+++ Twopointoh/wp-content/themes/twopointoh/single.php
@@ -63,8 +63,46 @@ while (have_posts()):
     </div>
 
 
+<?php
+    $rec_article_args = array(
+        'post_type'     => 'post',
+        'post_status'   => 'publish',
+        'order'         => 'desc',
+        'order_by'      =>  'date',
+        'category_name' => $category_name,
+        'post__not_in'  => [get_the_ID()]
+    );
 
+$rec_articles = new WP_Query( $rec_article_args);
+if($rec_articles){
+    ?>
 
+<div class="rec-slider">
+<div class="normal-width">
+<h2>Related work</h2>
+</div>
+
+<?php
+foreach ($rec_articles -> posts as $article):
+    $category = get_the_category($article);
+    $category_name = $category[0]->name;
+    $main_colour = get_field('post_main_colour',$article);
+    ?>
+    <a class ="archive-post-link" href="<?php echo esc_url(get_the_permalink( $article))?>">
+        <div class="thumb-container">
+        <img src="<?php echo esc_url(get_the_post_thumbnail_url( $article));?>" alt="">
+        <div class="thumb-category"><span class="thumb-cat"><?php esc_html_e($category_name)?></span><span class="thumb-excerpt"><?php echo wp_kses_post(get_the_excerpt($article))?></span></div>
+        </div>
+        <div class="thumb-info">  
+            <div class="thumb-name"><?php esc_html_e(get_the_title( $article))?></div>
+        </div>
+
+    </a>
+    <?php
+    endforeach;
+}
+    ?>
+    </div>
     </article>
 <?php
 endwhile;
